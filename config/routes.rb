6da1040f425b4f73a8c7dd7ebe078f3398ed8803
sasaharukimedes <sasaharu3@gmail.com>
2022-04-14 20:55:43 +0900

    add devise mvc

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'pages#home'
+  get 'pages/about'
+  get 'pages/play'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
