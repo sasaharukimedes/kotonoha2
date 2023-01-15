fe9ee33815c6d86a75f6160869ac6e32ce37cfc2
sasaharukimedes <sasaharu3@gmail.com>
2023-01-15 21:05:15 +0900

    add just one sentence

--- spec/requests/posts_apis_spec.rb
+++ spec/requests/posts_apis_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe "PostsApis", type: :request do
   before(:each) do
     5.times do |user|
       @user = create(:user)
+      #@receiver = User.where.not(id:@user.id).order(:received_at).first
     end
     @receiver = User.where.not(id:@user.id).order(:received_at).first
     #@user = create(:user)
@@ -18,6 +19,7 @@ RSpec.describe "PostsApis", type: :request do
 
   #https://qiita.com/nakachan1994/items/f62cd4d93c061e3fa6df参考
   let!(:post) {create(:post, user_id: @user.id , sender_id: @user.id, receiver_id: @receiver.id)}
+
   #let!(:post) {create(:post, user_id: other_user.id , sender_id: other_user.id, receiver:user, receiver_id: other_user.id)}
 
   #describe "GET /posts_apis" do
@@ -32,10 +34,10 @@ RSpec.describe "PostsApis", type: :request do
     it "returns http success" do
       sign_in @user
       get new_post_path
+      p Post.last
       expect(response).to have_http_status(:success)
     end
   end
-
 
   #ポストの作成ののち、received_atが更新されているのか確認
 
