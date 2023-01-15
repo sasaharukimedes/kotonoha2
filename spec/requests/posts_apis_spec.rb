2c0175af2b740f57a989eeb37e89dfb040eaabbb
sasaharukimedes <sasaharu3@gmail.com>
2023-01-15 22:02:25 +0900

    add for next test

--- spec/requests/posts_apis_spec.rb
+++ spec/requests/posts_apis_spec.rb
@@ -1,24 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "PostsApis", type: :request do
-  #let!(:user) { create(:user) }
-  #let!(:other_user) { create(:user) }
-  #let!(:alice) {create(:alice)}
-
   before(:each) do
     5.times do |user|
       @user = create(:user)
-      #@receiver = User.where.not(id:@user.id).order(:received_at).first
     end
     @receiver = User.where.not(id:@user.id).order(:received_at).first
-    #@user = create(:user)
-    #@other_user = create(:other_user)
-    #@alice=create(:alice)
-    #@receiver = @other_user
   end
 
   #https://qiita.com/nakachan1994/items/f62cd4d93c061e3fa6df参考
-  let!(:post) {create(:post, user_id: @user.id , sender_id: @user.id, receiver_id: @receiver.id)}
+  3.times do
+    let!(:post) {create(:post, user_id: @user.id , sender_id: @user.id, receiver_id: @receiver.id)}
+  end
 
   #let!(:post) {create(:post, user_id: other_user.id , sender_id: other_user.id, receiver:user, receiver_id: other_user.id)}
 
@@ -34,11 +27,14 @@ RSpec.describe "PostsApis", type: :request do
     it "returns http success" do
       sign_in @user
       get new_post_path
-      p Post.last
       expect(response).to have_http_status(:success)
     end
+
+    #ポストの作成ののち、received_atが更新されているのか確認
+    it "is expected that received_at is updated" do
+      #p Post.all
+      #p User.all.order(received_at: :desc)
+    end
+
   end
-
-  #ポストの作成ののち、received_atが更新されているのか確認
-
 end
