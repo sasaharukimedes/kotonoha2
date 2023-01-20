7a568c2a592e4c6158df909d7317c5e952ecb47e
sasaharukimedes <sasaharu3@gmail.com>
2023-01-20 22:33:44 +0900

    add post#create request test and pass that, but I have no confidence.

--- spec/requests/posts_apis_spec.rb
+++ spec/requests/posts_apis_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe "PostsApis", type: :request do
   end
 
   #https://qiita.com/nakachan1994/items/f62cd4d93c061e3fa6df参考
-  let!(:posts) { create_list(:post, 3, user_id: @user.id , sender_id: @user.id, receiver_id: @receiver.id) }
+  let!(:new_post) { create(:post, user_id: @user.id , sender_id: @user.id, receiver_id: @receiver.id) }
 
   #let!(:post) {create(:post, user_id: other_user.id , sender_id: other_user.id, receiver:user, receiver_id: other_user.id)}
 
@@ -25,15 +25,23 @@ RSpec.describe "PostsApis", type: :request do
     it "returns http success" do
       sign_in @user
       get new_post_path
-      get new_post_path
+      expect(response).to have_http_status(:success)
+      p Post.all
+    end
+  end
+
+  describe "POST / create" do
+    it "returns http success" do
+      sign_in @user
+      post "/posts", :params => {:post => {
+        :user_id =>  @user.id ,
+        :sender_id => @user.id,
+        :receiver_id => @receiver.id
+      }}
+      p Post.all
       expect(response).to have_http_status(:success)
     end
-
-    #ポストの作成ののち、received_atが更新されているのか確認
-    it "is expected that received_at is updated" do
-      p Post.all
-      #p User.all.order(received_at: :desc)
-    end
-
   end
+
 end
+
