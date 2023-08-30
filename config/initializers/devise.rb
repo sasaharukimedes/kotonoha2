063cba6dd3b2a28697cd99f34794244e7be03ea6
SASAKI Haruki <sasaharu3@gmail.com>
2023-08-30 13:35:23 +0900

    Update devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -40,7 +40,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'updating.harukimedes@gmail.com'
+  config.mailer_sender = 'xxxxx'
 
 
   # Configure the class responsible to send e-mails.
